1711c671a94eaa4426d3b853ab9455f6525c588c
Iliya-Kiporiev <73113918+woomy144@users.noreply.github.com>
2021-10-31 02:06:06 +0600

    Delete NET.JS directory

--- NET.JS/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed('#element', {
-    // Waits 1000ms after typing "First"
-    strings: ['Привет', 'Мы клан коноха', 'Большой клан на проекте Onyx', 'Присоединяйся', 'https://discord.gg/e8zcR2vvwZ'],
-    typeSpeed: 50,
-    backSpeed: 30,
-    loop: true,
-});  
